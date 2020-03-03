e13c3e2fc5b0e3019e753941d99f92563297d3df
Lukas Bowden <T00040951@mytru.ca>
2020-03-03 15:14:08 -0800

    Initial commit

---
+++ app/src/main/java/com/example/simplyattendance/newCourse.java
@@ -0,0 +1,14 @@
+package com.example.simplyattendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_course);
+    }
+}
